f87ea13d40e42a48ca3bcd5d14fcd860eb2d4737
DharaniSingaram <112459382+DharaniSingaram@users.noreply.github.com>
2022-12-23 17:01:20 -0500

    Update password_validity.js

--- password_validity.js
+++ password_validity.js
@@ -1,14 +1,12 @@
-function CheckPassword(inputtxt) 
-{ 
-var passw=  /^[A-Za-z]\w{7,14}$/;
-if(inputtxt.value.match(passw)) 
-{ 
-alert('Correct, try another...')
-return true;
-}
-else
-{ 
-alert('Wrong...!')
-return false;
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function CheckPassword(inputtxt) {
+    var passw =
+        /^[A-Za-z]\w(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{16}$/;
+    if (inputtxt.value.match(passw)) {
+        alert("Correct, try another...");
+        return true;
+    } else {
+        alert("Wrong...!");
+        return false;
+    }
 }
