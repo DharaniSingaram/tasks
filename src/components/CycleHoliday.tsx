43d4d39caa5f5f35bbd9716c91abc45639d9baba
dharanisingaram <dharani.singaram@maine.edu>
2023-01-05 14:52:35 -0500

    Solved Cycle Holiday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,55 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    // Declare a state variable called "holidays" that is an array of strings representing the holidays
+    const holidays = [
+        { name: "New Year's Day", emoji: "🎉" },
+        { name: "Valentine's Day", emoji: "💖" },
+        { name: "Halloween", emoji: "🎃" },
+        { name: "Thanksgiving", emoji: "🦃" },
+        { name: "Christmas", emoji: "🎄" }
+    ];
+
+    // Declare a state variable called "currentHoliday" and initialize it to "New Year's Day"
+    const [currentHoliday, setCurrentHoliday] = useState(holidays[0]);
+
+    // Declare a function called "cycleAlphabetically" that will be called when the "Alphabet" button is clicked
+    const cycleAlphabetically = () => {
+        // Find the index of the current holiday in the "holidays" array
+        const currentIndex = holidays.findIndex(
+            (holiday) => holiday.name === currentHoliday.name
+        );
+
+        // Calculate the next index by incrementing the current index and wrapping around to the beginning of the array if necessary
+        const nextIndex = (currentIndex + 1) % holidays.length;
+
+        // Set the current holiday to the holiday at the next index
+        setCurrentHoliday(holidays[nextIndex]);
+    };
+
+    const cycleByYear = () => {
+        // Find the index of the current holiday in the "holidays" array
+        const currentIndex = holidays.findIndex(
+            (holiday) => holiday.name === currentHoliday.name
+        );
+
+        // Calculate the next index by incrementing the current index and wrapping around to the beginning of the array if necessary
+        const nextIndex = (currentIndex + 1) % holidays.length;
+
+        // Set the current holiday to the holiday at the next index
+        setCurrentHoliday(holidays[nextIndex]);
+    };
+
+    return (
+        <div>
+            {/* Display the current holiday name and emoji */}
+            <div>
+                Holiday: {currentHoliday.name} {currentHoliday.emoji}
+            </div>
+            {/* Render the "Alphabet" button with the "cycleAlphabetically" function as the onClick handler */}
+            <Button onClick={cycleAlphabetically}>Alphabet</Button>
+            {/* Render the "Year" button with the "cycleByYear" function as the onClick handler */}
+            <Button onClick={cycleByYear}>Year</Button>
+        </div>
+    );
 }
