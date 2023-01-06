1badbe3d0609bc27a7372c263db87d743490b6f4
dharanisingaram <dharani.singaram@maine.edu>
2023-01-05 23:02:19 -0500

    solved TwoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,45 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [d1, setD1] = useState<number>(0);
+    const [d2, setD2] = useState<number>(1);
+
+    const rollLeft = (): void => {
+        setD1(d6());
+    };
+
+    const rollRight = (): void => {
+        setD2(d6());
+    };
+
+    return (
+        <div>
+            <div>
+                <Button onClick={rollLeft}>Roll Left</Button>
+                <Button onClick={rollRight}>Roll Right</Button>
+            </div>
+            <div>
+                <span
+                    data-testid="left-die"
+                    dangerouslySetInnerHTML={{ __html: d1.toString() }}
+                ></span>
+            </div>
+            <div>
+                <span
+                    data-testid="right-die"
+                    dangerouslySetInnerHTML={{ __html: d2.toString() }}
+                ></span>
+            </div>
+            {d1 === 1 && d2 === 1 && (
+                <div data-testid="game-result">
+                    <span>Lose</span>
+                </div>
+            )}
+            {d1 === d2 && d1 !== 1 && (
+                <div data-testid="game-result">
+                    <span>Win</span>
+                </div>
+            )}
+        </div>
+    );
 }
